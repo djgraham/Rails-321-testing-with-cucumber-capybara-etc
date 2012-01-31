97b1fbe80be03c387c4502d69b6e579de66db0a9
David Graham <github@djgraham.co.uk>
2012-01-31 03:52:58 +0000

    Initial 3.2.1 import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railscast155::Application.config.secret_token = '86c4d3109380778d7f95561cb6f3c0706199d0a83eeb96fb92cd3dff004834c803403037f69bf9b630d37917583fd3f8049b259d020d704f660705f03a133599'
